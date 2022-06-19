26efc2979e65bd5463326e861923f6c98b26a04b
egg-corn <timothywan.ky@gmail.com>
2022-06-19 20:58:16 +0800

    Added a dropdown menu to the website, allowing for potential additional functionality in the future. Also allowed for multiple 5 stars.

--- out/main.js
+++ out/main.js
@@ -1,11 +1,21 @@
 import { calculateChance } from "./Calculator.js";
-export function main() {
-    document.getElementById("output").value = "";
-    console.log("main called");
-    calculateChance();
-}
+const functionality = document.querySelector('#functionality');
+functionality.addEventListener('change', () => {
+    if ((functionality.value) == "featured-5-stars") {
+        document.getElementById("5-pity").disabled = false;
+        document.getElementById("5-guarantee").disabled = false;
+    }
+    else {
+        console.log("Not Implemented!");
+    }
+});
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
+    if (functionality.value == "featured-5-stars") {
+        calculateChance();
+    }
+    else {
+        console.log("Not Implemented!");
+    }
     e.preventDefault();
-    main();
 });
